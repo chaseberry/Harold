2f5b2db2f84cea95ad7d53abaf62a534d3ed5600
laxperson808 <laxperson808@gmail.com>
2013-09-30 18:55:10 -0400

    filters out non-mp3s and Harold/harold
    Added multiple dirs Harold and harold are now both accepted
    also removes non-mp3s from the list of music if you put in in the folder

--- harold.php
+++ harold.php
@@ -1,10 +1,33 @@
 <?php
-$files = array_diff(scandir("./Harold/"), array('..','.'));
-$music = "./Harold/" . $files[array_rand($files,1)];
-if(file_exists($music)){
 
-header("Content-Type:audio/x-mp3");
-readfile($music);
+$files = array_merge(getMusic("./Harold/"),getMusic("./harold/"));	//make the array from the two folders
+
+for($z = 0; $z < count($files); $z++){
+	$temp = explode(".", $files[$z]);					//gets the extension
+	if($temp[2] != "mp3"){						//check for mp3
+		unset($files[$z]);						//removes all non-mp3 files from array
+	}	
+}	
+
+$music = $files[array_rand($files,1)];					//sets the music file to play from Harold
+if(file_exists($music)){							//the music file exists
+	header("Content-Type:audio/x-mp3");				//set header type
+	readfile($music);							//play it
+}
+
+function getMusic($file){
+	if(file_exists($file)){//checks to see if the folder exists
+		$arr = array_diff(scandir($file), array('..','.'));	//makes the array
+		return addDir(array_merge($arr),$file);			//returns the array;
+	}
+	return array();							//returns a blank array so the system doesn't go BLEH if no file is found
+}
+
+function addDir($arr, $file){
+	for($z=0;$z<count($arr);$z++){					
+		$arr[$z] = $file . $arr[$z];				//adds the correct directory for each file
+	}
+	return $arr;
 }
 
 ?>
